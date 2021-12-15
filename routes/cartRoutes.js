a2512bfe38845451380ae03fa8b23631fe4695f0
Sean Zimbelman <Szimbe295@west-mec.local>
2021-12-15 16:08:50 -0700

    remove items in cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,10 +4,11 @@ const {
   getAllItems,
   removeItem,
   updateItem,
+  removeAllItems,
 } = require("../controllers/cartController");
 const router = express.Router();
 
-router.route("/").post(createItem).get(getAllItems);
+router.route("/").post(createItem).get(getAllItems).delete(removeAllItems);
 router.route("/:id").delete(removeItem).post(updateItem);
 
 module.exports = router;
